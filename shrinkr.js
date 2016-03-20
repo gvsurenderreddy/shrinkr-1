34e5d3612abd7ce63e5b71b5695cbfef0a81b68e
Grigør <grigor@xachatryan.com>
2016-03-20 16:16:59 +0400

    update

--- shrinkr.js
+++ shrinkr.js
@@ -70,6 +70,6 @@ app.get('/:encoded_id', function(req, res){
 
 });
 
-var server = app.listen(3000, function(){
+var server = app.listen(6000, function(){
   console.log('Server listening on port 3000');
 });
